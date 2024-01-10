718b502d0257e7caaac2b0d96abd74e5846707dc
Hashem Sami <hashem.sf@gmail.com>
2024-01-10 20:08:07 +0300

    finsing login form to set the bloc pattern

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -5,10 +5,46 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      margin: EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
       child: Column(
-        children: [],
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: const EdgeInsets.only(bottom: 30.0),
+          ),
+          submitButton(),
+        ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return const TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: "you@example.com",
+        labelText: "Email Address",
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: "Password",
+        labelText: "Password",
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () => {},
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+      ),
+      child: const Text("Login"),
     );
   }
 }
