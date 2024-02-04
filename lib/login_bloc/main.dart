0409d5032e593fadc2a35128190ca21aaed73250
Hashem Sami <hashem.sf@gmail.com>
2024-02-04 05:27:57 +0300

    starting the animation example

---
+++ lib/login_bloc/main.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+import "src/app.dart";
+
+// define the main  function
+void main() {
+  runApp(const App());
+}
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,25 +1,19 @@
 import "package:flutter/material.dart";
-import "./screens/login_screen.dart";
-import './blocs/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // making the provider bloc available to the child widgets
-    // so now every widgit in the tree can reach reach back up to the
-    // tree and get data from this provider
-    return Provider(
-        key: key,
-        child: MaterialApp(
-          title: "logged in",
-          home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Log Me In!!"),
-            ),
-            body: LoginScreen(),
-          ),
-        ));
+    return MaterialApp(
+      title: "Animation",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Log Me In!!"),
+        ),
+        body: const Text("data"),
+      ),
+    );
   }
 }
